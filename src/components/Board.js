520279dc3e913a379c3793e7c6474d0e2334a061
Chad Ellison and Haleigh Rogers <pair+cellz+hroge@allstate.com>
2018-06-14 14:40:03 -0500

    Build squares in a 10x10 board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import '../styles/board.css';
+import Square from './Square';
+
+const rows = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 export default class Board extends Component {
+  renderRow(row) {
+    let gameRow = columns.map((column, index)=> {
+        return <Square key={'square'+index.toString()}
+          id={(row+column).toString()}/>
+      })
+    return <div className="Row" key={row}>{gameRow}</div>
+  }
+
+  renderBoard() {
+    return(
+      rows.map((row, index)=> {
+        return this.renderRow(row)
+      })
+    )
+  }
+
   render() {
     return(
-      <div className="Board-space">
-      
+      <div className="Board">
+        {this.renderBoard()}
       </div>
     )
   }
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/square.css';
+
+export default class Square extends Component {
+
+  render() {
+    return(
+      <div className="Square"></div>
+    )
+  }
+}
